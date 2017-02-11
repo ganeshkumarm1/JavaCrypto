0d507ee49f2dd733cfd56dcf613b8cd035c6ca80
GaNeShKuMaRm <ganeshkumar.m1996@gmail.com>
2017-02-11 13:24:53 +0000

    DES partial completion

--- DES/src/HELPERS.java
+++ DES/src/HELPERS.java
@@ -4,9 +4,32 @@
 * @Last Modified by:   GaNeShKuMaRm
 * @Last Modified time: 2017-01-13 19:25:29
 */
+import java.math.BigInteger;
 
 public class HELPERS
 {
+    public String padZeros(String stringBIN)
+    {
+        if(stringBIN.length() == 4)
+        {
+            return stringBIN;
+        }
+        if(stringBIN.length() == 3)
+        {
+            return "0" + stringBIN;
+        }
+        if(stringBIN.length() == 2)
+        {
+            return "00" + stringBIN;
+        }
+        if(stringBIN.length() == 1)
+        {
+            return "000" + stringBIN;
+        }
+        return stringBIN;   
+    }
+    
+    
     public String asciiToHex(String string)
     {
         String stringHEX = "";
@@ -20,11 +43,31 @@ public class HELPERS
     public String hexToBinary(String stringHEX)
     {
         String stringBIN = "";
-       // stringBIN = Long.toBinaryString(Long.parseLong(stringHEX, 16));
         for(int index = 0; index < stringHEX.length(); index ++)
         {
-            stringBIN += Integer.toBinaryString(Integer.parseInt(Character.toString(stringHEX.charAt(index)), 16));
+            String temp = Integer.toBinaryString(Integer.parseInt(Character.toString(stringHEX.charAt(index)), 16));
+            stringBIN += padZeros(temp);
         }
         return stringBIN;
+    }
+    
+    public String binaryToHex(String stringBIN)
+    {
+        BigInteger decimal = new BigInteger(stringBIN, 2);
+        BigInteger hex = new BigInteger(decimal, 10);
+        //System.out.println(hexStr);
+        return new String(hex.toByteArray());//hexStr;
+    }
+    
+    public String hexToAscii(String stringHEX)
+    {
+        String str = "";
+        for(int i = 0; i < stringHEX.length() ; i += 2)
+        {
+            String s = stringHEX.substring(i, (i + 2));
+            int decimal = Integer.parseInt(s, 16);
+            str = str + (char) decimal;
+        }          
+        return str;
     }
 }
